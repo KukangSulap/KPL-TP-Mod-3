091751b2e48e6d141f6c6794a33d82a2d9bf2384
Howler Nouvelle <hilmy@wahidin.net>
2022-03-11 18:02:04 +0700

    menambahkan implementasi dengan state-based construction

--- app/src/main/java/com/barney/kpltpmod3/Soal2.java
+++ app/src/main/java/com/barney/kpltpmod3/Soal2.java
@@ -3,6 +3,9 @@ package com.barney.kpltpmod3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Soal2 extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Soal2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soal2);
+
+        TextView txtState = findViewById(R.id.txtState);
+        txtState.setText("idle");
+
+        Button btn1 = findViewById(R.id.btnBuka);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtState.setText("terbuka");
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btnKunci);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtState.setText("terkunci");
+            }
+        });
     }
 }
